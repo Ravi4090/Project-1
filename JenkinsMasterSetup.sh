6eb3a080c53711d3c31b53e587e5e39dc080cebe
Ravi Shankar Rajupalepu <148464895+Ravi4090@users.noreply.github.com>
2024-05-03 19:30:11 +0530

    Create JenkinsMasterSetup.sh

---
+++ JenkinsMasterSetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update -y
+
+# Install OpenJDK 17 JRE
+sudo apt install openjdk-17-jre -y
+
+# Install Jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+# Install Jenkins
+sudo apt-get install jenkins -y
